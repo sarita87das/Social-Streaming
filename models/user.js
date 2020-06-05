d1d4c98439932fea40a0be1b7382715f76fc19bb
Matt <mbatko0914@gmail.com>
2020-06-04 21:46:18 -0400

    updated User

--- models/user.js
+++ models/user.js
@@ -4,6 +4,11 @@ var bcrypt = require('bcryptjs');
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     // The email cannot be null, and must be a proper email before creation
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -16,8 +21,43 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: true
     }
-  });
+  },
+  {
+    validate: {
+      checkLoc() {
+        if(this.country === null && this.province !== null || this.city !== null)
+          throw new Error('Cannot select City or State unless Country is selected!');
+        if(this.province === null && this.city !== null)
+          throw new Error('Cannot select City unless State is selected');
+    }
+  }
+});
+    User.associate = function (models) {
+      // We're saying that a favorite tv show or movie should belong to a user
+      //
+      User.hasMany(models.favorites, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+    return movieShow;
+  };
+
+
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
