fd44f39f4946c5023fbbb13973524c97d9f51eeb
j.bennett <61255945+jben817@users.noreply.github.com>
2020-06-06 12:02:30 -0400

    changes to models

--- models/favorites.js
+++ models/favorites.js
@@ -1,36 +1,36 @@
-module.exports = function (sequelize, DataTypes) {
-  var Favorite = sequelize.define("Favorites", {
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    movieShowId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-  });
+// module.exports = function (sequelize, DataTypes) {
+//   var Favorite = sequelize.define("Favorites", {
+//     userId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       validate: {
+//         len: [1]
+//       }
+//     },
+//     movieShowId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       validate: {
+//         len: [1]
+//       }
+//     },
+//   });
 
-  Favorite.associate = function (models) {
-    // We're saying that a favorite tv show or movie should belong to a user
-    // 
-    Favorite.belongsToMany(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-    Favorite.belongsToMany(models.MovieShow, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
+//   Favorite.associate = function (models) {
+//     // We're saying that a favorite tv show or movie should belong to a user
+//     // 
+//     Favorite.belongsToMany(models.User, {
+//       foreignKey: {
+//         allowNull: false
+//       }
+//     });
+//     Favorite.belongsToMany(models.MovieShow, {
+//       foreignKey: {
+//         allowNull: false
+//       }
+//     });
+//   };
 
-  return Favorite;
-};
+//   return Favorite;
+// };
 
--- models/movieShow.js
+++ models/movieShow.js
@@ -1,33 +1,33 @@
-  module.exports = function (sequelize, DataTypes) {
-    var MovieShow = sequelize.define("MovieShow", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      service: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-    });
+  // module.exports = function (sequelize, DataTypes) {
+  //   var MovieShow = sequelize.define("MovieShow", {
+  //     title: {
+  //       type: DataTypes.STRING,
+  //       allowNull: false,
+  //       validate: {
+  //         len: [1]
+  //       }
+  //     },
+  //     service: {
+  //       type: DataTypes.STRING,
+  //       allowNull: false,
+  //       validate: {
+  //         len: [1]
+  //       }
+  //     },
+  //   });
   
-    MovieShow.associate = function (models) {
-      // We're saying that a favorite tv show or movie should belong to a user
-      // 
-      MovieShow.hasMany(models.Favorite, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
+  //   MovieShow.associate = function (models) {
+  //     // We're saying that a favorite tv show or movie should belong to a user
+  //     // 
+  //     MovieShow.hasMany(models.Favorite, {
+  //       foreignKey: {
+  //         allowNull: false
+  //       }
+  //     });
+  //   };
   
-    return MovieShow;
-  };
+  //   return MovieShow;
+  // };
   
   
   
