196f0c7b161f149ac019d53bd75e6750979726e6
j.bennett <61255945+jben817@users.noreply.github.com>
2020-06-16 14:15:55 -0400

    changes to user_profile and movie search

--- public/js/user_search.js
+++ public/js/user_search.js
@@ -4,39 +4,46 @@ $(document).ready(function () {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
-        var userName = $('#user-input').val().trim();
+        var addedUser = $('#user-input').val().trim();
 
 
         // Send the POST request.
-        $.post('/api/user', { 'username': userName })
+        $.post('/api/user', { 'username': addedUser })
             .then(function (result) {
                 console.log(result);
                 for (var i = 0; i <= 10; i++) {
-                    var uName = result[i].username;
+                    var uName = result[i].username
                     var uID = result[i].id;
 
                     $('#userData').append(`
           <div
           <p>Username: ${uName}</p>
           <p>User Id: ${uID}</p>
-          <button id = "followUser" type="button" class="btn btn-primary">Follow</button>
+          <button id = "followUser" type="button" class="btn btn-primary" data-id="user-id">Follow</button>
           </div>
           `);
+                    console.log(result[1].username);
 
-               }
+                }
             });
     })
 })
 
 
-// $("#followuser").on("click", function (event) {
-//     // Make sure to preventDefault on a submit event.
-//     event.preventDefault();
+$("#followUser").on("click", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    var userId = $(this).data("user-id");
 
-//     var followID = 
 
-
-//     // Send the POST request.
-//     $.post('/api/follow/:otherUser', { 'username': userName })
-//         .then(function (result) {
-//             console.log(result);
+    // Send the POST request.
+    $.post('/api/follow/:' + userId)
+        .then(
+            function () {
+                console.log("userfollowed");
+                alert("user followed!")
+                // Reload the page
+                location.reload();
+            }
+        );
+});
