f7707c4c8b868f12ca0842f359df51f98b61233d
sarita87das <nutankarna87@gmail.com>
2020-06-26 17:31:33 -0400

    changes made to follows.js

--- models/follows.js
+++ models/follows.js
@@ -1,19 +1,19 @@
 module.exports = function (sequelize) {
   var Follows = sequelize.define('Follows', {
-    followingId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    followerId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    }
+    // followingId: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1]
+    //   }
+    // },
+    // followerId: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1]
+    //   }
+    // }
   });
   Follows.associate = function(models) {
     Follows.belongsTo(models.User, { foreignKey: 'id', as: 'followerId'} );
